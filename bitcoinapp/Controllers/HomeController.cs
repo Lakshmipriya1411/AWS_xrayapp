6e5ad966814374b8dbc1cd2ac10307d18521733d
Lakshmi1411 <lakshmi.gundlapalli@sjsu.edu>
2021-09-20 14:59:21 -0700

    Add project files.

---
+++ bitcoinapp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Amazon.XRay.Recorder.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Amazon.XRay.Recorder.Handlers.AwsSdk;
+using Amazon.XRay.Recorder.Handlers.SqlServer;
+using Amazon.XRay.Recorder.Handlers.System.Net;
+
+namespace bitcoinapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            AWSXRayRecorder.Instance.TraceMethod("Outgoing Http Request", About);
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ bitcoinapp/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using Amazon.XRay.Recorder.Handlers.AspNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace bitcoinapp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public override void Init()
+        {
+            base.Init();
+            AWSXRayASPNET.RegisterXRay(this, "ASPNET test");
+
+        }
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
